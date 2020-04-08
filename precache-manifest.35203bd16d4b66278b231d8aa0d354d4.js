c2c3767f8657258547e2642a98ca94bf5b1806d9
Kürşat Yiğitoğlu <kursat@pirireis.com.tr>
2020-04-08 20:38:18 +0300

    Updates

---
+++ precache-manifest.35203bd16d4b66278b231d8aa0d354d4.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b51daf7e0c795b753a59184617704b35",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1db60d23041868e704fe",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "468a2fe5797a03b1c755",
+    "url": "/static/js/2.f33f8142.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.f33f8142.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1db60d23041868e704fe",
+    "url": "/static/js/main.0da5a527.chunk.js"
+  },
+  {
+    "revision": "f15a88a2018ebf0162f0",
+    "url": "/static/js/runtime-main.e600126a.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
